d1d7bbca0adc4cd32ae7b347fada95aa8b314acd
JuanSalvi <91103822+JuanSalvi@users.noreply.github.com>
2023-03-30 16:29:15 -0600

    Codigos

---
+++ PlayerControllerX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 19.0f;
+    public float rotationSpeed = 55.0f;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Siempre se mueve hacia adelante (Error de Ir hacia atras y Rapido)
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys (Error de Rotacion Rapida)
+        transform.Rotate(Vector3.left, Time.deltaTime * rotationSpeed * verticalInput);
+        
+        // ORIGINAL -transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+    }
+}
